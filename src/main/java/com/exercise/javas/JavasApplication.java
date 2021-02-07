371a7c7ff19ae430436558639e463914ffd4c1a6
boxak <boxak@naver.com>
2021-02-07 23:35:33 +0900

    2021-02-07 그냥 다시 jsp 쓰는 걸로...

--- src/main/java/com/exercise/javas/JavasApplication.java
+++ src/main/java/com/exercise/javas/JavasApplication.java
@@ -3,11 +3,17 @@ package com.exercise.javas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootConfiguration
 @SpringBootApplication
-public class JavasApplication {
+public class JavasApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(JavasApplication.class, args);
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(JavasApplication.class);
     }
 }
